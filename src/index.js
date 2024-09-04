07a5031c1b5bd0b02a9277abdd35f02caff87fd5
samuelsen <santossamuelsouza@gmail.com>
2024-09-04 02:23:42 -0300

    Desafio do Felipao

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log ('oi')
+let fruta = 'banana'
+
+switch (fruta){ 
+    case "morando": 
+    console.log('Essa fruta é o morango')
+    break 
+
+    case "uva": 
+    console.log('Essa fruta é uva')
+    break 
+
+    case "banana": 
+    console.log ('Essa fruta é banana')
+    break 
+    
+}
